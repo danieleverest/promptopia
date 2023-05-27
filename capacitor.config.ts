52bd5b46b89dfb1deac4c344f56b78b8883606b8
SHAD AFRIDI <shadafridi03@gmail.com>
2023-05-28 04:30:20 +0530

    fix: added capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.example.app",
+  appName: "promptopia",
+  webDir: "dist",
+  server: {
+    url: "0.0.0.0", // Run your next.js server and use url here for api routes
+  },
+};
+
+export default config;
